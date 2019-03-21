ca7016c4ad0b4d6b5ab78d1009a5945ceaff38a0
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-21 10:23:18 -0700

    makes port dynamic and adds start script

---
+++ Notes.md
@@ -0,0 +1,11 @@
+## Continuous Deployment
+
+1. Create app
+2. Deploy > Click on GitHub
+3. 'App connected to GitHub' > 'Connect to GitHub' > search name of repository
+4. Click 'Connect'
+5. Authorize Heroku - click 'Approve'
+6. 'Automatic deploys' > 'Enable Automatic Deploys'
+7. 'Manual deploy' > click 'Deploy branch'
+8. Click 'Overview' tab to see deployment progress
+9. if you preview your app and see an application error, it usually means you haven't pushed
--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const server = require('./api/server.js');
+require("dotenv").config();
+const server = require("./api/server.js");
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+// step 1: make the port dynamic
+const port = process.env.PORT || 4000;
+const greeting = process.env.GREETING;
+// node apps run on process
+// process has an obj called env
+// most services will add a variable to .env
+
+// step 2: create .env
+// PORT=5000
+
+// gotcha: need to restart server
+
+// step 3: yarn add dotenv
+// express doesn't know how to read .env
+
+// step 4: require dotenv as early as possible
+
+server.listen(port, () => {
+  console.log(`\n*** ${greeting} http://localhost:${port} ***\n`);
 });
